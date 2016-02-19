2907ce1cd47934234241751e55d3b56d6ad2fd88
chameleonbr <chameleonbr@gmail.com>
2016-02-19 14:58:45 -0200

    Revert "duhh"
    This reverts commit 032477f279f0a8272b80119d1158a6c4c0642ffd.

--- line.js
+++ line.js
@@ -1,6 +1,7 @@
 module.exports = function (RED) {
     var Liner = require('liner');
 
+
     function ReadLine(n) {
         RED.nodes.createNode(this, n);
         this.name = n.name;
@@ -17,7 +18,6 @@ module.exports = function (RED) {
         });
 
         node.on('input', function (msg) {
-            var file = null;
             if (msg.filename !== undefined && msg.filename !== "") {
                 file = msg.filename;
             } else {
@@ -63,4 +63,6 @@ module.exports = function (RED) {
 
     }
     RED.nodes.registerType("read-line", ReadLine);
+
+
 };
